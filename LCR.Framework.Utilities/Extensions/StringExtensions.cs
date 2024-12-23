cc8cd969b372930730634b4541ec40faf072ea74
Luan Caetano Rechi <luanlcr@hotmail.com>
2024-12-23 12:16:02 -0300

    adicionado extensões de string

---
+++ LCR.Framework.Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace LCR.Framework.Utilities.Extensions;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrEmpty(this string value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+
+    public static bool IsNullOrWhiteSpace(this string value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+
+    public static string NullToEmpty(this string value)
+    {
+        return value ?? "";
+    }
+
+    public static string Join(this IEnumerable<string> collection, string separator)
+    {
+        return string.Join(separator, collection);
+    }
+}
