6b09e59791a2103d6561e90b424ae0c8d7f460c7
Luan Caetano Rechi <luanlcr@hotmail.com>
2024-12-23 10:56:08 -0300

    #12 adicionado extensão de serialização e desserialização json

---
+++ LCR.Framework.Utilities/Extensions/JsonExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace LCR.Framework.Utilities.Extensions;
+
+public static class JsonExtensions
+{
+    public static string ToJson<T>(this T input, JsonSerializerSettings settings = null)
+    {
+        return JsonConvert.SerializeObject(input, settings);
+    }
+
+    public static T JsonTo<T>(this string json, JsonSerializerSettings settings = null)
+    {
+        return JsonConvert.DeserializeObject<T>(json, settings);
+    }
+}
