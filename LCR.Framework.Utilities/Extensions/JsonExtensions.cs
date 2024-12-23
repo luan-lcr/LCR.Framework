6fbf43d1c843d03dae418765be4dea567e8dba09
Luan Caetano Rechi <luanlcr@hotmail.com>
2024-12-23 11:55:54 -0300

    #13 adicionado serialização e desserialização XML

--- LCR.Framework.Utilities/Extensions/JsonExtensions.cs
+++ LCR.Framework.Utilities/Extensions/JsonExtensions.cs
@@ -4,12 +4,12 @@ namespace LCR.Framework.Utilities.Extensions;
 
 public static class JsonExtensions
 {
-    public static string ToJson<T>(this T input, JsonSerializerSettings settings = null)
+    public static string ToJson<T>(this T input, JsonSerializerSettings settings = null) where T : class
     {
         return JsonConvert.SerializeObject(input, settings);
     }
 
-    public static T JsonTo<T>(this string json, JsonSerializerSettings settings = null)
+    public static T JsonTo<T>(this string json, JsonSerializerSettings settings = null) where T : class
     {
         return JsonConvert.DeserializeObject<T>(json, settings);
     }
